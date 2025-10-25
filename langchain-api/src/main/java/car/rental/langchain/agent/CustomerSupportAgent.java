352bd2e88012cfe3ce2dc5550f13206f8ad03513
Sanjin Dumancic <sanjin.dumancic@yahoo.com>
2025-10-25 17:43:15 +0200

    added injection detection to chat

--- langchain-api/src/main/java/car/rental/langchain/agent/CustomerSupportAgent.java
+++ langchain-api/src/main/java/car/rental/langchain/agent/CustomerSupportAgent.java
@@ -2,10 +2,13 @@ package car.rental.langchain.agent;
 
 import car.rental.langchain.domain.model.CoreUser;
 import car.rental.langchain.infrastructure.persistence.CancelReservationRepository;
+import car.rental.langchain.service.PromptInjectionGuard;
 import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.guardrail.InputGuardrails;
 import io.quarkiverse.langchain4j.RegisterAiService;
 import io.quarkiverse.langchain4j.ToolBox;
 import jakarta.enterprise.context.SessionScoped;
+import org.eclipse.microprofile.faulttolerance.*;
 
 @SessionScoped
 @RegisterAiService
@@ -15,10 +18,26 @@ public interface CustomerSupportAgent {
             You are a customer support agent of a car rental company 'Miles of Smiles'.
             You are friendly, polite and concise.
             If the question is unrelated to car rental, you should politely redirect the customer to the right department.
+            If user asks question in his local locale language, respond in the same language.
             
             Today is {current_date}.
             The logged in user is {user}.
             """)
     @ToolBox(CancelReservationRepository.class)
+    @InputGuardrails(PromptInjectionGuard.class)
+    @Timeout(5000)
+    @Retry(maxRetries = 3, delay = 100)
+    @Fallback(CustomerSupportAgentFallback.class)
     String chat(@dev.langchain4j.service.UserMessage String message, @dev.langchain4j.service.V("user") CoreUser user);
+
+    public static class CustomerSupportAgentFallback implements FallbackHandler<String> {
+
+        private static final String EMPTY_RESPONSE = "Failed to get a response from the AI Model. Are you sure it's up and running, and configured correctly?";
+
+        @Override
+        public String handle(ExecutionContext context) {
+            return EMPTY_RESPONSE;
+        }
+
+    }
 }
--- langchain-api/src/main/java/car/rental/langchain/service/CustomerSupportAgentWebSocket.java
+++ langchain-api/src/main/java/car/rental/langchain/service/CustomerSupportAgentWebSocket.java
@@ -3,6 +3,7 @@ package car.rental.langchain.service;
 import car.rental.langchain.agent.CustomerSupportAgent;
 import car.rental.langchain.domain.model.CoreUser;
 import car.rental.langchain.infrastructure.restclient.CoreApiClientWrapper;
+import dev.langchain4j.guardrail.InputGuardrailException;
 import io.quarkus.websockets.next.OnOpen;
 import io.quarkus.websockets.next.OnTextMessage;
 import io.quarkus.websockets.next.WebSocket;
@@ -33,7 +34,14 @@ public class CustomerSupportAgentWebSocket {
 
     @OnTextMessage
     public String onMessage(String message) {
-        // Optionally, you can use userId here if needed
-        return customerSupportAgent.chat(message, coreUser);
+        try {
+            return customerSupportAgent.chat(message, coreUser);
+        } catch (InputGuardrailException e) {
+            log.error("Error calling the LLM: {}", e.getMessage());
+            return "Sorry, I am unable to process your request at the moment. It's not something I'm allowed to do.";
+        } catch (Exception e) {
+            log.error("Error calling the LLM: {}", e.getMessage());
+            return "I ran into some problems. Please try again.";
+        }
     }
 }
